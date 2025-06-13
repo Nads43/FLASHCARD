12602274093c19a712a972dd380ff9f67e86806b
Nads43 <daniel.nadalin@escola.pr.gov.br>
2025-06-13 00:23:38 -0300

    Add files via upload

---
+++ cartas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Física',
+    'Qual a unidade de medida da corrente elétrica?',
+    'Ampère (A).'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual organela é conhecida como a "usina de energia" da célula?',
+    'Mitocôndria.'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Deodoro da Fonseca (proclamou a República em 1889).'
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que significa a sigla "HTML" em computação?',
+    'HyperText Markup Language (Linguagem de Marcação de Hipertexto).'
+)
+
+criaCartao(
+    'Estados da Matéria',
+    'Como se chama a mudança de estado líquido para gasoso?',
+    'Evaporação (ou ebulição, se for rápida).'
+)
+
+criaCartao(
+    'Anatomia',
+    'Quantos ossos tem o corpo humano adulto?',
+    '206 ossos.'
+)
+
+criaCartao(
+    'Curiosidade Científica',
+    'Por que o céu é azul?',
+    'Por causa do espalhamento da luz solar (dispersão de Rayleigh), que faz com que o azul seja mais visível.'
+)
+
+criaCartao(
+    'Conhecimento geral',
+    'O Buiu gosta do 2D?',
+    'Lógico, sua sala favorita (te amamos buiu).'
+)
